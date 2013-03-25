a9c6415d567bc6f1903f3882c253640376b40243
shgfl007 <shgfl@hotmail.com>
2013-03-25 17:07:38 -0400

    objs can be eaten!!! NO DRAGGING!

--- testApp.h
+++ testApp.h
@@ -6,30 +6,52 @@
 #include "ofxBox2d.h"
 #include "stack.h"
 #include <stack>
-class testApp : public ofxiPhoneApp{
-	
-    public:
-        void setup();
-        void update();
-        void draw();
-        void exit();
-	
-        void touchDown(ofTouchEventArgs & touch);
-        void touchMoved(ofTouchEventArgs & touch);
-        void touchUp(ofTouchEventArgs & touch);
-        void touchDoubleTap(ofTouchEventArgs & touch);
-        void touchCancelled(ofTouchEventArgs & touch);
 
-        void lostFocus();
-        void gotFocus();
-        void gotMemoryWarning();
-        void deviceOrientationChanged(int newOrientation);
+#define N_SOUNDS 5
+
+class SoundData {
+public:
+    int soundID;
+    bool bHit;
+};
+
+class testApp : public ofxiPhoneApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    void touchDown(ofTouchEventArgs & touch);
+    void touchMoved(ofTouchEventArgs & touch);
+    void touchUp(ofTouchEventArgs & touch);
+    void touchDoubleTap(ofTouchEventArgs & touch);
+    void touchCancelled(ofTouchEventArgs & touch);
+    
+    void lostFocus();
+    void gotFocus();
+    void gotMemoryWarning();
+    void deviceOrientationChanged(int newOrientation);
+    void contactStart(ofxBox2dContactArgs &e);
+    void contactEnd(ofxBox2dContactArgs &e);
+    
     
     ofxBox2d box2d;
     vector<ofxBox2dCircle> circles;
     vector<ofPolyline> curves;
+    
+    float counter;
+    float counter2;
+    float counter3;
+    float counter4;
+    float counter5;
+    float counter6;
+    float counter7;
+    
+    //sound objs
+    ofSoundPlayer sound[N_SOUNDS];
     //vector<Carnivore> carnivores;
     //stack<Carnivore> Cs;
 };
-
 
--- testApp.mm
+++ testApp.mm
@@ -1,13 +1,28 @@
 #include "testApp.h"
 
 
+int r = 255;
+int g = 255;
+int b = 255;
+
+
+int fr = 200; int fg = 241; int fb = 70;
+int sr = 146; int sg = 196; int sb=109;
+int tr = 40; int tg = 126; int tb = 125;
+int fourr = 35; int fourg = 73; int fourb = 111;
+int fiver = 35; int fiveg = 91; int fiveb = 147;
+int sixr = 23; int sixg=182; int sixb = 211;
+int sevenr =4; int seveng = 216; int sevenb= 255;
+
+
+
 int myCircleX;
 int myCircleY;
 int countC = 0;
 int countH = 0;
 int countP = 0;
 int countTotal = 0;
-int Hrank_STANDARD = 2;
+int Hrank_STANDARD = 1;
 
 
 vector<ofxBox2dRect> carnivores;
@@ -16,55 +31,68 @@ vector<ofxBox2dPolygon> plants;
 
 
 //eat function, please keep the higher rank creature as A!!!!!!!
-void eat(ofxBox2dBaseShape A, ofxBox2dBaseShape B)
+bool eat(ofxBox2dBaseShape A, ofxBox2dBaseShape B)
 {
-    if (A.rank > B.rank && A.hungerM < 3 && A.rank > Hrank_STANDARD && B.rank >0) {
+    if (A.rank > B.rank && A.hungerM < 300 && A.rank > Hrank_STANDARD && B.rank >0) {
         //A is carnivore, b is either carnivore or herbivore
-        B.alive = false;
-        
         if (B.rank<=Hrank_STANDARD && B.rank >0) {
             //B is a Herbivore
-            countH--;
-            A.hungerM++;
+            A.hungerM+=200;
+            return true;
+            
         }
         else {
             // B is a Carnivore
-            countC--;
-            A.hungerM+=2;
+            A.hungerM+=200;
+            return true;
         }
         
     }
-    else if(A.rank > B.rank && A.rank<=Hrank_STANDARD && A.rank>0 && B.rank==0 && A.hungerM < 3){
+    else if(A.rank > B.rank && A.rank==Hrank_STANDARD && B.rank==0 && A.hungerM < 300){
         // A is a Herbivore, B is a plant
-        B.alive = false;
-        countP--;
-        A.hungerM++;
+        A.hungerM+=200;
+        return true;
+        
     }
+
     
-   
+    return false;
 }
 
+//--------------------------------------------------------------
+
+bool bodiesAreTouching(b2Body* body1, b2Body* body2){
+    for (b2ContactEdge* edge = body1->GetContactList(); edge; edge=edge->next) {
+        if ( !edge->contact->IsTouching() )
+            continue;
+        b2Body* bA = edge->contact->GetFixtureA()->GetBody();
+        b2Body* bB = edge->contact->GetFixtureB()->GetBody();
+        if ( ( bA == body1 && bB == body2 ) || ( bB == body1 && bA == body2 ) )
+            return true;
+    }
+    return false;
+}
 
 
 //--------------------------------------------------------------
 void testApp::setup(){
-	
+    
     ofRegisterTouchEvents(this);
     ofxAccelerometer.setup();
     ofxiPhoneAlerts.addListener(this);
-	ofSetFrameRate(30);
-
+    ofSetFrameRate(30);
     
-	myCircleX=300;
+    
+    myCircleX=300;
     myCircleY = 200;
-	//If you want a landscape oreintation 
-	//iPhoneSetOrientation(OFXIPHONE_ORIENTATION_LANDSCAPE_RIGHT);
-	
-	ofBackgroundHex(0x8c8377);
-    ofxMultiTouch.addListener(this);
-
+    //If you want a landscape oreintation
+    //iPhoneSetOrientation(OFXIPHONE_ORIENTATION_LANDSCAPE_LEFT);
     
-    //add-on testing
+    ofBackgroundHex(0x8c8377);
+    ofxMultiTouch.addListener(this);
+    
+    
+    
     
     box2d.init();
     box2d.setGravity(0, 0.1);
@@ -73,48 +101,387 @@ void testApp::setup(){
     box2d.createBounds();
     box2d.setIterations(1, 1); // minimum for IOS
     
-    for (int i=0; i<10; i++) {
-        ofxBox2dCircle c;
-        c.setPhysics(1, 0.4, 0.4);
-        c.setup(box2d.getWorld(), ofRandomWidth(), ofRandomHeight(), ofRandom(13, 25));
-        circles.push_back(c);
+    //register the listener so that we get the events
+    ofAddListener(box2d.contactStartEvents, this, &testApp::contactStart);
+    ofAddListener(box2d.contactEndEvents, this, &testApp::contactEnd);
+    
+    counter = 0.0;
+    counter2 = 0.0;
+    counter3 =0.0;
+    counter4 =0.0;
+    counter5=0.0;
+    counter6=0.0;
+    counter7=0.0;
+    
+    
+    //load sound
+    for (int i=0; i<N_SOUNDS; i++) {
+        
+        sound[i].loadSound("sfx/" + ofToString(i)+ ".mp3");
+        sound[i].setMultiPlay(true);
+        sound[i].setLoop(false);
     }
-    //add-on testing ends here
+}
+
+//----------------------------------------------------------------------
+
+void testApp::contactStart(ofxBox2dContactArgs &e){
+    if (e.a != NULL && e.b != NULL) {
+        
+        //two carnivores
+        if (e.a->GetType() == b2Shape::e_circle && e.b->GetType() == b2Shape::e_circle) {
+            if (e.a->GetBody()->GetUserData()) {
+                
+                SoundData * aData = (SoundData*)e.a->GetBody()->GetUserData();
+                SoundData * bData = (SoundData*)e.b->GetBody()->GetUserData();
+                
+                if (aData) {
+                    aData->bHit = true;
+                    sound [aData->soundID].play();
+                }
+                
+                if (bData) {
+                    bData->bHit = true;
+                    sound[bData->soundID].play();
+                }
+            }
+        }
+        
+        
+    }
+}
+
+//--------------------------------------------------------------
+
+void testApp::contactEnd(ofxBox2dContactArgs &e){
+    if (e.a != NULL && e.b !=NULL) {
+        SoundData * aData = (SoundData*)e.a->GetBody()->GetUserData();
+        SoundData * bData = (SoundData*)e.b->GetBody()->GetUserData();
+        
+        if (aData) {
+            aData->bHit = false;
+        }
+        
+        if (bData) {
+            bData->bHit = false;
+        }
+    }
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
- 
     
+    
+    bool isEaten;
     //add-on testing
     ofVec2f gravity = ofxAccelerometer.getForce();
-    //gravity.y *= ( -1 * ofRandom(-1,1));
-    //gravity.x *= (-1 * ofRandom(-1,1));
+    
     
     gravity *= 5;
-
+    
     box2d.setGravity(gravity);
     box2d.update();
-    //add-on testing ends here
+    vector<ofxBox2dRect> _tempCEat;
+    vector<ofxBox2dCircle> _tempHCEat;
+    vector<ofxBox2dPolygon> _tempPHEat;
+    vector<ofxBox2dRect> _tempC;
+    vector<ofxBox2dCircle> _tempH;
+    vector<ofxBox2dPolygon> _tempP;
+    
+    //---------------collision detect and eat herbivores and plants---------------------
+    for (vector<ofxBox2dCircle>::iterator it = herbivores.begin(); it!=herbivores.end(); ++it) {
+        for (vector<ofxBox2dPolygon>::iterator it2 = plants.begin(); it2!=plants.end(); ++it2) {
+            bool isTouching = bodiesAreTouching(it->body, it2->body);
+            if (isTouching) {
+                isEaten = eat(*it, *it2);
+                if (isEaten) {
+                    it2->exist=false;
+                }
+            }
+        }
+    }
+    for (vector<ofxBox2dPolygon>::iterator it = plants.begin(); it!=plants.end(); ++it) {
+        if (it->exist == false) {
+            it->destroy();
+            countP--;
+        }else _tempPHEat.push_back(*it);
+    }
+    plants = _tempPHEat;
+
+    //---------------collision detect and eat herbivores and carnivores---------------------
+    for (vector<ofxBox2dCircle>::iterator it = herbivores.begin(); it!=herbivores.end(); ++it) {
+        for (vector<ofxBox2dRect>::iterator it2 = carnivores.begin(); it2!=carnivores.end(); ++it2) {
+            bool isTouching = bodiesAreTouching(it->body, it2->body);
+            if (isTouching) {
+                    isEaten = eat(*it2, *it);
+                    if (isEaten) {
+                        it->exist=false;
+                    }
+                }
+            }
+    }
+    
+    for (vector<ofxBox2dCircle>::iterator it = herbivores.begin(); it!=herbivores.end(); ++it) {
+        if (it->exist == false) {
+            it->destroy();
+            countH--;
+        }else _tempHCEat.push_back(*it);
+    }
+    herbivores = _tempHCEat;
+    
+    //---------------collision detect and eat carnivores-------------------------------------
+    for (vector<ofxBox2dRect>::iterator it = carnivores.begin(); it!=carnivores.end(); ++it) {
+        for (vector<ofxBox2dRect>::iterator it2 = carnivores.begin(); it2!=carnivores.end(); ++it2) {
+            bool isTouching = bodiesAreTouching(it->body, it2->body);
+            if (isTouching) {
+                if (it->rank>it2->rank) {
+                    isEaten = eat(*it, *it2);
+                    if (isEaten) {
+                        it2->exist = false;
+                        }
+                }else
+                {
+                    isEaten = eat(*it2, *it);
+                    if (isEaten) {
+                        it->exist=false;
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+    for (vector<ofxBox2dRect>::iterator it = carnivores.begin(); it!=carnivores.end(); ++it) {
+        if (it->exist == false) {
+            it->destroy();
+            countC--;
+        }else _tempCEat.push_back(*it);
+    }
+    
+    carnivores=_tempCEat;
+    //----------------refresh the stack------------------------------------------------------
+    
     for(vector<ofxBox2dRect>::iterator it = carnivores.begin(); it != carnivores.end(); ++it) {
-        it->hungerM-=0.5;
+       
+        it->hungerM-=0.0000000005;
+        if(it->hungerM==0)it->exist=false;
+        if (it->exist==false) {
+            it->destroy();
+            countC--;
+        }else _tempC.push_back(*it);
     }
     
     for(vector<ofxBox2dCircle>::iterator it = herbivores.begin(); it != herbivores.end(); ++it) {
-        it->hungerM-=0.5;
+        it->hungerM-=0.0000000005;
+        if(it->hungerM==0)it->exist=false;
+        if (it->exist==false) {
+            it->destroy();
+            countH--;
+        }else _tempH.push_back(*it);
     }
     
-    countTotal = countP + countH + countC;
+    for(vector<ofxBox2dPolygon>::iterator it = plants.begin(); it != plants.end(); ++it) {
+        it->hungerM-=0.0000000000005;
+        if(it->hungerM==0)it->exist=false;
+        if (it->exist==false) {
+            it->destroy();
+            countP--;
+        }else _tempP.push_back(*it);
+    }
+    
 
+    
+    
+    carnivores = _tempC;
+    herbivores = _tempH;
+    plants = _tempP;
+    
+    countTotal = countP + countH + countC;
+    
+    if(countTotal > 20 && r > 150 && g > 150 && b > 150 )
+    {
+        r--;g--;b--;
+    }
+    if(countTotal > 30 && r > 0 && g > 0 && b > 0 )
+    {
+        r--;g--;b--;
+    }
+    
+    if(countTotal > 20 && fb <=200 )
+    {
+        fb+=2;
+    }
+    if(countTotal > 30 && fr <= 230 && fb <= 230 )
+    {
+        fr++;fb++;
+    }
+    
+    if(countTotal > 20 && sr <=160 && sb <= 160 )
+    {
+        sr++;sb+=3;
+    }
+    if(countTotal > 30 && sr <= 196 && sb <= 196 )
+    {
+        sr++;sb+=3;
+    }
+    
+    if(countTotal > 20 && tr <=80)
+    {
+        tr++;
+    }
+    if(countTotal > 30 && tr <= 126)
+    {
+        tr++;
+    }
+    
+    if(countTotal > 20 && fourr < 90 && fourg <90)
+    {
+        fourr+=2; fourg++;
+    }
+    if(countTotal > 30 && fourr <= 111 && fourg <= 111)
+    {
+        fourr+=2; fourg++;
+    }
+    
+    if(countTotal > 20 && fiver < 90 )
+    {
+        fiver+=2;
+    }
+    if(countTotal > 30 && fiver <= 147 && fiveg <= 147)
+    {
+        fiver++; fiveg++;
+    }
+    
+    if(countTotal > 20 && sixr < 182 )
+    {
+        sixr+=2;
+    }
+    if(countTotal > 30 && sixr <= 211 && sixg <= 211)
+    {
+        sixr++; sixg++;
+    }
+    if(countTotal > 20 && sevenr < 216 )
+    {
+        sevenr+=2;
+    }
+    if(countTotal > 30 && sevenr <= 255 && seveng <= 255)
+    {
+        sevenr++; seveng++;
+    }
+    
+    
+    //add 0.029 to our counter
+    counter = counter + 0.010f;
+    counter2 = counter2 + 0.015f;
+    counter3 += 0.020f;
+    counter4 += 0.025f;
+    counter5 += 0.030f;
+    counter6 += 0.035f;
+    
+    counter7 += 0.040f;
+    
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
     
-
+    float k = 0.0;
     
-    //add-on testing
+    
+    
+    
+    
+    ofSetColor(r, g, b);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter7 - k) + 30.0));
+        k += 0.4;
+    }
+    
+    
+    
+    
+    
+    //blue
+    ofSetColor(sevenr, seveng, sevenb);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter7 - k) + 15.0));
+        k += 0.4;
+    }
+    
+    //dark purple
+    
+    ofSetColor(sixr, sixg, sixb);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter6 - k) + 14.0));
+        k += 0.4;
+    }
+    //purple
+    ofSetColor(fiver, fiveg, fiveb);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter5 - k) + 13.0));
+        k += 0.4;
+    }
+    //red
+    
+    
+    ofSetColor(fourr, fourg, fourb);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter4 - k) + 12.0));
+        k += 0.4;
+        
+    }
+    //orange
+    ofSetColor(tr, tg, tb);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter3 - k) + 11.0));
+        k += 0.4;
+    }
+    
+    //yellow
+    //This is doing it again but for a different color
+    
+    ofSetColor(sr, sg, sb);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter2 - k) + 9.0));
+        k += 0.4;
+    }
+    
+    
+    
+    //green
+    ofSetColor(fr, fg, fb);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter - k) + 8.0));
+        k += 0.4;
+    }
+    
+    ofSetColor(242, 242, 242);
+    k = 0;
+    for(int i = 0; i < ofGetWidth(); i+= 50)
+    {
+        ofRect(i+5, ofGetHeight(), 50, -50 * (sin(1.4 * counter2 - k) + 2.0));
+        k += 0.2;
+    }
+    
+    
+    //draw carnivores-------------------------------------------
     
     for(vector<ofxBox2dRect>::iterator it = carnivores.begin(); it != carnivores.end(); ++it) {
         if (it->rank==2) {
@@ -126,21 +493,27 @@ void testApp::draw(){
         else if(it->rank == 4){
             ofSetHexColor(0x8E0B00);
         }
+        SoundData * data = (SoundData*)it->getData();
+        if (data && data->bHit) {
+            ofSetHexColor(0xff0000);
+        }
         it->draw();
+        
     }
     
+    
+    //draw herbivores------------------------------------------
     ofSetHexColor(0xCFDB79);
     for(vector<ofxBox2dCircle>::iterator it = herbivores.begin(); it != herbivores.end(); ++it) {
         it->draw();
     }
     
     ofSetHexColor(0x27855E);
-    ofFill();
-    for(int i = 0; i<curves.size(); i++)
-    {
-        curves[i].draw();
-        
-    }
+    /*ofFill();
+     for(int i = 0; i<curves.size(); i++)
+     {
+     curves[i].draw();
+     }*/
     for (int i = 0; i < plants.size(); i++) {
         plants[i].draw();
     }
@@ -148,13 +521,14 @@ void testApp::draw(){
     ofSetColor(90);
     ofDrawBitmapString("double tap to add more", 20, 30);
     ofDrawBitmapString(ofToString(ofGetFrameRate(), 0)+" fps", 20, 50);
+    ofDrawBitmapString(ofToString(countTotal, 0), 20, 70);
     //add-on testing ends here
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::exit(){
-
+    
 }
 
 //--------------------------------------------------------------
@@ -163,24 +537,25 @@ void testApp::touchDown(ofTouchEventArgs & touch){
     {
         ofxBox2dCircle h;
         h.setPhysics(0.1, 0.4, 1);
-        h.setup(box2d.getWorld(), touch.x, touch.y, ofRandom(10,15));
+        int r = ofRandom(10,15);
+        int x = touch.x;int y = touch.y;
+        h.setup(box2d.getWorld(), x, y, r);
         herbivores.push_back(h);
         countH++;
+        
+        
     };
     
     if (touch.id == 2) {
         curves.push_back(ofPolyline());
         curves.back().addVertex(touch.x,touch.y);
     }
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::touchMoved(ofTouchEventArgs & touch){
-    /*ofxBox2dCircle h;
-    h.setPhysics(0.1, 0.4, 1);
-    h.setup(box2d.getWorld(), touch.x, touch.y, ofRandom(10,15));
-    herbivores.push_back(h);*/
+    
     if (touch.id == 2) {
         curves.back().addVertex(touch.x, touch.y);
     }
@@ -188,11 +563,8 @@ void testApp::touchMoved(ofTouchEventArgs & touch){
 
 //--------------------------------------------------------------
 void testApp::touchUp(ofTouchEventArgs & touch){
-    /*ofxBox2dPolygon p;
-    p.setPhysics(0.1, 0.4, 1);
-    p.setup(box2d.getWorld());
-    plant.push_back(p);*/
-
+    
+    
     if (touch.id == 2) {
         ofxBox2dPolygon plant;
         curves.back().simplify();
@@ -203,8 +575,9 @@ void testApp::touchUp(ofTouchEventArgs & touch){
         
         plant.create(box2d.getWorld());
         plants.push_back(plant);
+        countP++;
     }
-
+    
 }
 
 //--------------------------------------------------------------
@@ -231,21 +604,20 @@ void testApp::touchCancelled(ofTouchEventArgs & touch){
 
 //--------------------------------------------------------------
 void testApp::lostFocus(){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::gotFocus(){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::gotMemoryWarning(){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::deviceOrientationChanged(int newOrientation){
-
+    
 }
-
